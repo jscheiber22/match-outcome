93ccb2c18fd561fa1ae4aebf5e48d2417ba4109b
James Scheiber <jscheiber22@gmail.com>
2021-10-20 16:10:48 -0700

    initial commit after main file setup

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+	A script to take the possible match ups of a week of football and output all possible winners of each game.
+'''
+
+import tkinter as tk
+
+
+class MatchOutcome:
+
+	def __init__(self):
+		pass
+
+
+
+
+if __name__ == '__main__':
+	MatchOutcome()
