3230bf0d6bb91425ff188433597c642043646f9e
James Scheiber <jscheiber22@gmail.com>
2021-10-23 20:47:18 -0700

    now it only includes the teams that are selected via checkbox to help limit team possibilities

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 '''
 
 import tkinter as tk
+from tkinter import ttk
 from itertools import product
 import pandas as pd
 from itertools import product
@@ -17,27 +18,31 @@ class MatchOutcome:
 		self.window = tk.Tk()
 
 		self.window.rowconfigure(list(range(0,10)), minsize = 50, weight = 1)
-		self.window.columnconfigure(list(range(0,2)), minsize = 50, weight = 1)
+		self.window.columnconfigure(list(range(0,3)), minsize = 50, weight = 1)
 
 		self.IFTextFields = []
 
 		# Creates left side of panel for teams in favor
 		tk.Label(master=self.window, text="In Favor:").grid(row=0, column=0)
 		for x in range(1,9):
-			text = tk.Entry(master=self.window, width=30)
+			text = tk.Entry(master=self.window, width=20)
 			text.grid(row=x, column=0, sticky='nw')
 			text.bind("<Return>", self.outputSheet)
 			self.IFTextFields.append(text)
 
 		self.OFTextFields = []
+		self.checkButtons = []
 
 		# Creates right side of panel for teams out of favor
 		tk.Label(master=self.window, text="Out of Favor:").grid(row=0, column=2)
 		for x in range(1,9):
-			text = tk.Entry(master=self.window, width=30)
+			text = tk.Entry(master=self.window, width=20)
 			text.grid(row=x, column=2, sticky='ne')
 			text.bind("<Return>", self.outputSheet)
+			button = ttk.Checkbutton(master=self.window)
+			button.grid(row=x, column=3, sticky='e')
 			self.OFTextFields.append(text)
+			self.checkButtons.append(button)
 
 		# Submit button
 		tk.Button(self.window, text = 'SUBMIT', command=self.outputSheet).grid(row=9, column=1, sticky='n')
@@ -48,7 +53,8 @@ class MatchOutcome:
 	def outputSheet(self, event = None):
 		vsTeams = []
 		for x in range(0, 8):
-			vsTeams.append([self.IFTextFields[x].get(), self.OFTextFields[x].get()]) # creates a list that looks like [(team1, team2), (team3, team4)] for future processing
+			if self.checkButtons[x].instate(['selected']):
+				vsTeams.append([self.IFTextFields[x].get(), self.OFTextFields[x].get()]) # creates a list that looks like [(team1, team2), (team3, team4)] for future processing
 		combinations = product(*vsTeams)
 
 		df = pd.DataFrame(list(combinations))
